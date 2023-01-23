6518f4476053140690aa86a7a3253cd2ac94b6aa
Shaghayegh Ghavami <ghavami.shgh@gmail.com>
2023-01-23 22:09:52 +0330

    Remove unnecessary code

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Hello', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const text = screen.getByText(/Hello/i);
+  expect(text).toBeInTheDocument();
 });
