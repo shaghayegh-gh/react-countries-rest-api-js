b56be15868b5b534fff6f8a5a9a8f49b4482c782
Shaghayegh Ghavami <ghavami.shgh@gmail.com>
2023-01-24 12:32:50 +0330

    Add filters

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,6 @@
 import { useState, useEffect, useCallback } from "react";
 import { getCountries } from "../../services/countries-service";
+import Filters from "../../components/filters";
 import Countries from "../../components/countries";
 
 const Home = () => {
@@ -20,6 +21,7 @@ const Home = () => {
 
   return (
     <div className="md:container md:mx-auto px-4">
+        <Filters  setCountries={setCountries} setLoading={setLoading}/>
       <Countries countries={countries} loading={loading} />
     </div>
   );
